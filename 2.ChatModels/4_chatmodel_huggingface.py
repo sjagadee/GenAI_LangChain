26ae1977ad4da4dc3a4225889e33f7e9ace90162
sjagadee <srini.jagadeesh@gmail.com>
2025-10-18 18:12:51 -0500

    update with google and hugging face models

---
+++ 2.ChatModels/4_chatmodel_huggingface.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+)
+
+model = ChatHuggingFace(llm=llm)
+result = model.invoke("Which river is the longest river in USA")
+
+print(result.content)
